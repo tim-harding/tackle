fee04546b07a686e5a402339377fe4832334b37c
Tim Harding <Tim@TimHarding.co>
2024-11-04 20:58:41 -0800

    Reduce init/update/view to one function

--- functions/_tackle_inner.fish
+++ functions/_tackle_inner.fish
@@ -1,4 +1,8 @@
-function _tackle_inner
+function _tackle_inner --argument-names fn
+    if not set -q fn
+        return 1
+    end
+
     function fish_prompt
     end
     function fish_right_prompt
@@ -6,49 +10,57 @@ function _tackle_inner
     function fish_mode_prompt
     end
 
-    argparse "i/init=" "u/update=" "v/view=" -- $argv
-
-    if not set -q _flag_init _flag_update _flag_view
-        echo "Expected init, update, and view functions"
-        exit
+    for key in a b c d e f g h i j k l m n o p q r s t u v w x y z A B C D E F G H I J K L M N O P Q R S T U V W X Y Z 0 1 2 3 4 5 6 7 8 9 \$ \\ \* \? \~ \# \( \) \{ \} \[ \] \< \> \& \| \; \" \'
+        bind $key "_tackle_key $key"
     end
 
-    for key in a b c d e f g h i j k l m n o p q r s t u v w x y z A B C D E F G H I J K L M N O P Q R S T U V W X Y Z \ca \cb \cc \cd \ce \cf \cg \ch \ci \cj \ck \cl \cm \cn \co \cp \cq \cr \cs \ct \cu \cv \cw \cx \cy \cz \$ \\ \* \? \~ \# \( \) \{ \} \[ \] \< \> \& \| \; \" \'
-        bind $key "$_flag_update $key"
+    for key in \ca \cb \cc \cd \ce \cf \cg \ch \ci \cj \ck \cl \cm \cn \co \cp \cq \cr \cs \ct \cu \cv \cw \cx \cy \cz
+        bind $key "_tackle_key $key command"
     end
 
-    bind \e\[D "$_flag_update left"
-    bind \e\[C "$_flag_update right"
-    bind \e\[A "$_flag_update up"
-    bind \e\[B "$_flag_update down"
-    bind " " "$_flag_update space"
-    bind \r "$_flag_update enter"
-    bind \t "$_flag_update tab"
-    bind \e "$_flag_update escape"
-    bind \x7F "$_flag_update backspace"
-    bind \cC exit
+    bind \e\[D "_tackle_key left"
+    bind \e\[C "_tackle_key right"
+    bind \e\[A "_tackle_key up"
+    bind \e\[B "_tackle_key down"
+    bind " " "_tackle_key space"
+    bind \r "_tackle_key enter"
+    bind \t "_tackle_key tab"
+    bind \e "_tackle_key escape"
+    bind \x7F "_tackle_key backspace"
+    bind \cC exit 1
 
-    set -g _tackle_epoch 0
-    for var in $($_flag_init)
-        function _tackle_handle_state_$var --on-variable $var
-            set _tackle_epoch $(math $_tackle_epoch + 1)
-        end
-    end
-
-    function _tackle_view --on-variable _tackle_epoch --inherit-variable _flag_view
-        _tackle_erase to-end
-        set lines (string split \n ($_flag_view))
+    _tackle_cursor hide
+    while true
+        set lines ($fn $input)
         for line in $lines
             echo -e $line
         end
-        _tackle_cursor up (math 1 + (count lines))
-    end
 
-    function _tackle_exit --on-variable _tackle_exit
-        _tackle_cursor show
-        exit $_tackle_exit
-    end
+        if set -q _tackle_exit
+            _tackle_cursor show
+            exit $_tackle_exit
+        end
 
-    _tackle_cursor hide
-    _tackle_view
+        _tackle_cursor up (count $lines)
+
+        read --prompt "_tackle_cloak hide" input
+        _tackle_cloak show
+        _tackle_cursor line-up 1
+        _tackle_erase to-end
+        set_color normal
+    end
+end
+
+function _tackle_key
+    commandline --insert $argv
+    commandline --function execute
+end
+
+function _tackle_cloak
+    switch $argv[1]
+        case hide
+            echo -nes "\e[8m"
+        case show
+            echo -nes "\e[28m"
+    end
 end
